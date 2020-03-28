c76c2899ebd2219726a583eeec5fbbad427a9d59
Subhash Chandra <49841848+Subhash703@users.noreply.github.com>
2020-03-28 16:34:52 +0530

    Added comments for understanding of the code
    It is always a good idea to put comments in the code so that reader can understand the code easily.

--- SortestJobFirst_Assignement.c
+++ SortestJobFirst_Assignement.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h> 
- #define max = 30;
+
 int main() 
 { 
- 
+ //Variable declaration , 
   int count,i,j,m=0,n,y=0,time,remain=0,min,flag=0; 
-  int wait_time=0,turn_a_time=0,a_time[max],b_time[max],p[max],z[max];
+  int wait_time=0,turn_a_time=0,a_time[10],b_time[10],p[10],z[10];
   float k=0,x=0; 
-  printf("Enter number of Process:\t "); 
+  printf("Enter number of Process : "); 
   scanf("%d",&n); 
-  printf("\n\tArrival time should be greater than 2 as CPU remains idle for first 3 secs.\n");
-  printf("\n\tBurst time should be less tha 10\n");
+  printf("\nWARNINGS : \nArrival time should be greater than 2 as CPU remains idle for first 3 secs.");
+  printf("\nBurst time should be less tha 10\n\n");
   for(count=0;count<n;count++) 
   { 
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
+    printf("Enter Arrival Time and Burst Time for  Process Number %d :",count+1); 
     scanf("%d",&a_time[count]); 
     scanf("%d",&b_time[count]);  
   } 
 
+//Constraints checking
 for(i=0;i<n;i++)
 {
 	if(a_time[i]==0 )
@@ -44,36 +45,41 @@ for(i=0;i<n;i++)
 }
   printf("\n\n\tProcess\t|Turnaround Time|Waiting Time\n\n"); 
   printf("\t===========================================\n");
+  
 for(i=0;i<n;i++)
 {
 	m=m+b_time[i];
 }
 min=m;
 time=m;
+
+//Take the minimum burst time in time variable
 for(i=0;i<n;i++)
 {
 	if(a_time[i]<time)
 	{
-		time=a_time[i];
+		time=a_time[i];  
 	}
+	
 }
+//Now the time variable has the lowest burst time in all the process
 for(i=time;i<=m;i=i+b_time[j])
 {
 	min=m;
-	remain=0;
+	remain=0;  //Remaining burst time of the process
 	flag=0;
 
 	for(count=0;count<n;count++)
 	{
-	
+		//Arrival time of the process is less than time then 
 		if(a_time[count]<=i)
 		{
 
 			if(b_time[count]<min)
 			{
-
+				//If the burst time is less than the min burst time then process will execute completally and fleg = 1
 				min = b_time[count];
-				j=count;
+				j=count;		//J will tell that which process is executed now
 				flag=1;
 			}
 			remain=1;	 
@@ -81,11 +87,12 @@ for(i=time;i<=m;i=i+b_time[j])
 	}
 	if(flag==1&&remain==1)
 	{
+		//Print the data for the process j whihc is executed successfully
 		wait_time=i-a_time[j];
 		turn_a_time=wait_time+b_time[j];
 		printf("\tP[%d]\t|\t%d\t|\t%d\n",j+1,turn_a_time,wait_time); 
-		k=k+wait_time;
-		x=x+turn_a_time;
+		k=k+wait_time;			//Variable for storing the waiting time of the processes
+		x=x+turn_a_time;		//X is for storing the turn around time of the process
 	
 		a_time[j]=m+1;
 		p[y]=j+1;
@@ -93,12 +100,15 @@ for(i=time;i<=m;i=i+b_time[j])
 		y++;
 	}
 }
+
+//Print the average eaiting time & turn around time
 printf("\n\nAverage Waiting Time= %.2f\n",k/n); 
 printf("Avg Turnaround Time = %.2f",x/n); 
 printf("\n\n\nTotal time taken by processor to complete all the jobs : %d",m);  
 printf("\n\nQueue  for order of execution:\n");
-printf("\n\nProcess		");
 
+// Print the order of execution of the processes
+printf("\n\nProcess		");
 for(i=0;i<n;i++)
 {
 	printf(" P[%d]   ",p[i]);
@@ -107,7 +117,5 @@ for(i=0;i<n;i++)
 		printf("End");
 	}
 }
-
-
   return 0; 
 }
